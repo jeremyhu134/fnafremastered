7680d9d3f1c8417cb04bbbd63f31104a576ef61e
jeremyhu134 <52206356+jeremyhu134@users.noreply.github.com>
2022-11-04 12:53:21 -0400

    Update introNight.js

--- introNight.js
+++ introNight.js
@@ -8,12 +8,12 @@ class introNight extends Phaser.Scene {
     create() {
         gameState.intro = this.sound.add('intro');
         gameState.intro.play();
-        this.add.text( window.innerWidth/2-200,window.innerHeight/2-50, `Night${gameState.night}`, {fill: '#FFFFFF', fontSize: '60px'});
+        this.add.text( window.innerWidth/2-100,window.innerHeight/2-50, `Night${gameState.night}`, {fill: '#FFFFFF', fontSize: '60px'});
         var introtimer = this.time.addEvent({
             delay: 3000,
             callback: ()=>{
                 this.scene.stop('introNight');
-			    this.scene.start(`Night${gameState.night}`);
+		this.scene.start(`Night1`);
             },
             startAt: 0,
             timeScale: 1,
